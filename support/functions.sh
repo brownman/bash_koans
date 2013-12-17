a1b4d778f88d4bfbd9917cd876b821aaa5fece7a
brownman <ofer.shaham@gmail.com>
2013-12-17 16:00:27 +0200

    about_sed.sh - add koans

--- support/functions.sh
+++ support/functions.sh
@@ -8,7 +8,8 @@ green() {
 }
 
 red() {
-  echo -e "\033[31m$1\033[0m"
+  echo -e "\033[31m$1 $2\033[0m"
+  notify-send -t 20000 "$1" "$2"
 }
 
 
@@ -16,13 +17,14 @@ red() {
 assertEqual() {
   if [[ "$3" != "$4" ]]; then
     echo ''
-    red "  $1 has damaged your karma."
+    #notify-send ""
+    red "  $1" "error"
     echo ''
     echo "You have not yet reached enlightenment ..."
-    red  "  Expected '$3', got '$4'"
+    red  "  Expected '$3'" " got '$4'"
     echo ''
     echo "Please meditate on the following code:"
-    local filename=`grep  $1 src/* -l`
+    local filename=`grep  $1 src/*.sh -l`
     red  "  $filename:$2"
     echo ''
     # echo "You are now 10/291 koans and 2/36 lessons away from reaching enlightenment"
